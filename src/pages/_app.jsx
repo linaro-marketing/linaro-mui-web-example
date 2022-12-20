a31b59aa68034e2cbedbe8003d52f37d31f48988
Kyle Kirkby <kyle.kirkby@linaro.org>
2022-12-20 08:51:07 +0000

    Refactor / import components from linaro-mui-web

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,48 +1,29 @@
 import React from 'react';
 import ProgressBar from '@badrap/bar-of-progress';
 import Router from 'next/router';
-import { ThemeProvider } from '@context/ThemeContext';
-import CustomSnackbarProvider from '@components/CustomSnackbarProvider';
+import { CustomSnackbarProvider, ThemeProvider } from '@linaro-marketing/linaro-mui-web';
 import '@styles/global.css';
 import createCache from '@emotion/cache';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
-import { darkTheme } from '@styles/theme';
-// import * as Sentry from "@sentry/browser";
-// const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
-// const SENTRY_ENVIRONMENT = process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT;
-// const SENTRY_RELEASE = process.env.NEXT_PUBLIC_SENTRY_RELEASE;
-// if (typeof window !== "undefined") {
-//   console.log("Initialising Sentry with DSN", SENTRY_DSN);
-//   Sentry.init({
-//     dsn: SENTRY_DSN,
-//     release: SENTRY_RELEASE,
-//     environment: SENTRY_ENVIRONMENT,
-//     debug: true,
-//   });
-// }
-//---------- Setup progress bar.
-const primaryColour = darkTheme.palette.primary.main;
-// Grab the primary.main colour from the theme directly since this is outside
-// of the <ThemeProvider> scope
-const progress = new ProgressBar({
-	color: primaryColour,
-	className: 'bar-of-progress',
-	size: 4,
-	delay: 100,
-});
-Router.events.on('routeChangeStart', progress.start);
-Router.events.on('routeChangeComplete', progress.finish);
-Router.events.on('routeChangeError', progress.finish);
-//---------- End Setup progress bar.
+import { darkTheme, lightTheme } from '@styles/theme';
 const clientSideEmotionCache = createCache({ key: 'css', prepend: true });
-
 const App = function ({ Component, pageProps, emotionCache = clientSideEmotionCache }) {
 	const notistackRef = React.createRef();
+	const primaryColour = darkTheme.palette.primary.main;
+	const progress = new ProgressBar({
+		color: primaryColour,
+		className: 'bar-of-progress',
+		size: 4,
+		delay: 100,
+	});
+	Router.events.on('routeChangeStart', progress.start);
+	Router.events.on('routeChangeComplete', progress.finish);
+	Router.events.on('routeChangeError', progress.finish);
 	return (
 		<CacheProvider value={emotionCache}>
-			<ThemeProvider>
-				<CustomSnackbarProvider notistackRef={notistackRef}>
+			<ThemeProvider darkTheme={darkTheme} lightTheme={lightTheme}>
+				<CustomSnackbarProvider notistackRef={notistackRef} dismissText="dismiss">
 					<CssBaseline />
 					<Component {...pageProps} />
 				</CustomSnackbarProvider>
